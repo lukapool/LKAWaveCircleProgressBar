72b701ba3bbf166f2ba3a5721d06ab356fecbd82
Luka <422366176@qq.com>
2018-05-12 20:26:13 +0800

    bugs fix

---
+++ WaveAnimation/TestAViewController.h
@@ -0,0 +1,13 @@
+//
+//  TestAViewController.h
+//  WaveAnimation
+//
+//  Created by Luka on 2018/5/12.
+//  Copyright © 2018 Luka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestAViewController : UIViewController
+
+@end
---
+++ WaveAnimation/TestAViewController.m
@@ -0,0 +1,37 @@
+//
+//  TestAViewController.m
+//  WaveAnimation
+//
+//  Created by Luka on 2018/5/12.
+//  Copyright © 2018 Luka. All rights reserved.
+//
+
+#import "TestAViewController.h"
+
+@interface TestAViewController ()
+
+@end
+
+@implementation TestAViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
